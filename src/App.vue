97d2b3dc5b3f07160a34e36aa0150821af552db9
Nick Yang <nick.yang3721@gmail.com>
2018-10-22 22:30:28 -0400

    :sparkles: Add cube & basic animation

--- src/App.vue
+++ src/App.vue
@@ -22,11 +22,14 @@ export default {
       ),
 
       // Renderer: Displays scenes.
-      renderer: new THREE.WebGLRenderer()
+      renderer: new THREE.WebGLRenderer(),
+
+      // Cube: cube object to render.
+      cube: {}
     };
   },
   methods: {
-    // init() does inital set-up for three.js rendering.
+    // init does inital set-up for three.js rendering
     init: function() {
       // Canvas: Render area in the window.
       this.$data.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -34,11 +37,37 @@ export default {
 
       // camera defaults to middle, move it out a bit
       this.$data.camera.position.z = 5;
+    },
+    // createCube creates a cube & adds to canvas
+    createCube: function() {
+      // vertices and faces
+      let geometry = new THREE.BoxGeometry(1, 1, 1);
+
+      // material to display
+      let material = new THREE.MeshBasicMaterial({ color: 0x00ff0 });
+
+      // applies material & geometry
+      this.$data.cube = new THREE.Mesh(geometry, material);
+
+      this.$data.scene.add(this.$data.cube);
+    },
+    animate: function() {
+      // creates a loop 60 times/sec of the animate function
+      requestAnimationFrame(this.animate);
+
+      // increment done to cube, 60 times/sec
+      this.$data.cube.rotation.x += 0.02;
+      this.$data.cube.rotation.y += 0.01;
+
+      // render scene with camera.
+      this.$data.renderer.render(this.$data.scene, this.$data.camera);
     }
   },
-  // called
+  // called when App component mounts.
   mounted: function() {
     this.init();
+    this.createCube();
+    this.animate();
   }
 };
 </script>
